c50cc5e90b7579e700cc27ab8584381b8497d257
mdgulamasib <gulam.asib@gmail.com>
2022-05-22 21:56:49 +0600

    basic installation and db connection done

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xek1w.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('burlydB').collection('products');
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('burly is up and running!')
+})
+
+app.listen(port, () => {
+    console.log(`burly is listening on port ${port}`)
+})

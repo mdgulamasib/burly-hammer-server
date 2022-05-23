4503489d31919e17bf83f60bccf93d381bb02662
mdgulamasib <gulam.asib@gmail.com>
2022-05-23 23:01:44 +0600

    order update api created

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//jwtverify function 
+
+function JWTVerify(req, res, next) {
+    const authHeader = req.headers.authorization;
+    console.log(authHeader)
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Access Forbidden' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xek1w.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,6 +38,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('burlydB').collection('products');
+        const orderCollection = client.db('burlydB').collection('orders');
 
         //products loading
         app.get('/products', async (req, res) => {
@@ -27,6 +47,46 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
 
+        })
+
+        //purchase single data loading
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+
+        // updating available quantity
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQ = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    availableQ: updateQ.newAvailableQ,
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        //adding orders in collection
+        app.post("/orders", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
+
+
+        // JWT TOken auth connection
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
         })
     }
     finally {

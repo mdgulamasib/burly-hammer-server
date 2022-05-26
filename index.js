a2f375aa82ec43c5f0818a79083fa8c0e612078c
mdgulamasib <gulam.asib@gmail.com>
2022-05-26 17:54:16 +0600

    heroku issue

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
-const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xek1w.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 //jwtverify function 
 
@@ -29,9 +36,6 @@ function JWTVerify(req, res, next) {
 
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xek1w.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run() {
